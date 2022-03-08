af8278e1f97baa18bc2947c20a28620e20254460
Haroon Feisal <haroonfeisal@microsoft.com>
2022-03-08 14:23:57 -0500

    Removed random imports.

--- src/containerapp/azext_containerapp/_clients.py
+++ src/containerapp/azext_containerapp/_clients.py
@@ -2,9 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-from ast import NotEq
-from email import header
 import json
 import time
 import sys
